193782d8cf9991f20f0140fe2531c48ac77e5720
vital <vitalikmoskov233@gmail.com>
2023-06-30 12:40:45 +0300

    update api repository

--- app/src/main/java/com/example/deliveryapp/data/local/repository/UserRepository.kt
+++ app/src/main/java/com/example/deliveryapp/data/local/repository/UserRepository.kt
@@ -1,10 +1,16 @@
 package com.example.deliveryapp.data.local.repository
 
 import androidx.room.Dao
+import androidx.room.Query
+import com.example.deliveryapp.data.local.model.UserModel
 
 @Dao
 interface UserRepository {
 
+    @Query("select * from user")
+    suspend fun getUser(): UserModel
 
+    @Query("delete from user")
+    suspend fun deleteUser()
 
 }
--- app/src/main/java/com/example/deliveryapp/data/local/repositoryImpl/UserRepositoryImpl.kt
+++ app/src/main/java/com/example/deliveryapp/data/local/repositoryImpl/UserRepositoryImpl.kt
@@ -1,4 +1,14 @@
 package com.example.deliveryapp.data.local.repositoryImpl
 
-class UserRepositoryImpl {
+import com.example.deliveryapp.data.local.repository.UserRepository
+import javax.inject.Inject
+
+class UserRepositoryImpl @Inject constructor(
+    private val userRepository: UserRepository
+) {
+
+    suspend fun getUser() = userRepository.getUser()
+
+    suspend fun deleteUser() = userRepository.deleteUser()
+
 }
